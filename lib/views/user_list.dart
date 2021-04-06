1fb361dd73c8093f4d5a803a7586547b69184fe0
Marcos Baia <cs.marcosbaia@gmail.com>
2021-04-06 12:06:37 -0300

    Update user_list.dart

--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -3,10 +3,15 @@ import 'package:flutter/material.dart';
 class UserList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const users = {...DUMMY_USERS};
     return Scaffold(
       appBar: AppBar( 
         title: Text('Lista de Usuários'),
-      ),
-    );
+      ),//AppBar
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (ctx,i) => UserTile(users.values.elementAt(i)),
+        ),//ListView.builder
+    );//Scaffold
   }
 }
