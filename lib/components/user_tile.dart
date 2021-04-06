b0564a53566ad77ba407d60c6491e873741761a0
Marcos Baia <cs.marcosbaia@gmail.com>
2021-04-06 12:06:25 -0300

    Create user_tile.dart

---
+++ lib/components/user_tile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/models/user.dart';
+
+class User extends StatelessWidget{
+  final User user;
+
+  const UserTile(this.user);
+
+  @override
+  Widget build(BuildContext context){
+    final avatar = user.avatarUrl == null || user.avatarUrl.isEmpty
+    ? CircleAvatar(child: Icon(Icons.person))
+    : CircleAvatar(backgroundImage: NetworkImaage(user.avatarURL));
+    return ListTile(
+      leading: avatar,
+    );
+  }
+}
